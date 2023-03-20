24261849dcbaec24e5ecf7c4ac96a55f123f5949
Dzulfaridz-Ziziumiza <dzulfaridz.ziziumiza@gmail.com>
2023-03-20 14:22:57 +0800

    Continue a bit on improving the js.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 function buttonHandler(){
     // compile all the IDs
-    let idArray = ["username", "emailAddress", "package", "date", "amount", ];
+    let idArray = ["username", "emailAddress", "package", "date", "amount", "discount", "gender"];
 
     let userName = document.getElementById(idArray[0]).value;
     let emailAddress = document.getElementById(idArray[1]).value;
+    let package = document.getElementById(idArray[2]).value;
+    
 
     // use this for debugging
     console.log(emailAddress);
